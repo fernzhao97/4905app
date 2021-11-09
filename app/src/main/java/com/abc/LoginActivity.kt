d1810408603f3691843b3f6a6fc379dfb2cc7e05
Fern <fernzhaofei@gmail.com>
2021-11-08 20:03:05 -0800

    login and register complated, button and link click complated

--- app/src/main/java/com/abc/LoginActivity.kt
+++ app/src/main/java/com/abc/LoginActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 
 class LoginActivity : AppCompatActivity() {
@@ -17,8 +18,26 @@ class LoginActivity : AppCompatActivity() {
             // Do something in response to button click
             val intent = Intent(this,RegisterActivity::class.java)
             startActivity(intent)
+            overridePendingTransition(R.anim.slide_in_left,R.anim.stay)
 
         }
+        val addIcon: ImageView = findViewById(R.id.addIcon)
+        addIcon.setOnClickListener {
+            // Do something in response to button click
+            val intent = Intent(this,RegisterActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(R.anim.slide_in_left,R.anim.stay)
+
+        }
+        val loginBtn: Button = findViewById(R.id.loginButton)
+        loginBtn.setOnClickListener {
+            // Do something in response to button click
+            val intent = Intent(this,HomeActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(R.anim.slide_in_left,R.anim.stay)
+
+        }
+
 
 
     }
--- app/src/main/java/com/abc/RegisterActivity.kt
+++ app/src/main/java/com/abc/RegisterActivity.kt
@@ -1,11 +1,43 @@
 package com.abc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+
+        val backLink: ImageView = findViewById(R.id.backLink)
+        backLink.setOnClickListener {
+            // Do something in response to button click
+            val intent = Intent(this,LoginActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(R.anim.slide_in_left,R.anim.stay)
+
+        }
+
+        val back2btn: TextView = findViewById(R.id.back2btn)
+        back2btn.setOnClickListener {
+            // Do something in response to button click
+            val intent = Intent(this,LoginActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(R.anim.slide_in_left,R.anim.stay)
+
+        }
+
+        val registerBtn: Button = findViewById(R.id.registerButton)
+        registerBtn.setOnClickListener {
+            // Do something in response to button click
+            val intent = Intent(this,HomeActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(R.anim.slide_in_left,R.anim.stay)
+
+        }
     }
 }
