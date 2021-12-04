f962ee6973af0d9960507bb8881201ba306f0f51
Fern <fernzhaofei@gmail.com>
2021-12-04 14:59:29 -0800

    change

---
+++ app/src/main/java/com/abc/QuickSortActivity.kt
@@ -0,0 +1,11 @@
+package com.abc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class QuickSortActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quick_sort)
+    }
+}
